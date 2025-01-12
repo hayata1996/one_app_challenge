8587049186b23114ac25ed4db2e490d178e10d16
hayata1996 <kk19960603hh@gmail.com>
2025-01-12 09:20:26 +0900

    delete unnesessary files

---
+++ README.md
@@ -0,0 +1,10 @@
+first_app: for first week
+second_app: for seconde week
+....too obvious 
+
+uv is used for dependencies management and virtual env. Follow steps below.
+1. make sure you have uv.lock and pyproject.toml.
+2. run `uv run`
+3. cd first_app/app
+4. run `uv run sushida.py`
+5. wait for a couple of seconds. You will see browser open up automatically.
--- first_app/app/sushida.py
+++ first_app/app/sushida.py
@@ -3,11 +3,9 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from PIL import Image, ImageOps
+from PIL import Image
 import pyocr
-import pyocr.builders
 from utils import logger
 
 logger = logger.get_logger(name=__name__, debug=True)
@@ -31,7 +29,6 @@ driver.set_window_size(*window)
 target_url = 'https://sushida.net/play.html'
 driver.get(target_url)
 
-# 寿司打のゲーム画面をずらすために書く
 # target_xpath = '//*[@id="gameContainer"]'
 webgl_element = driver.find_element(By.ID, "#canvas")
 logger.debug(f"webgl_element: {webgl_element}")
@@ -62,21 +59,6 @@ logger.debug(f"Canvas中央の座標: ({click_x}, {click_y})")
 action = ActionChains(driver)
 action.move_to_element_with_offset(webgl_element, 0, 30).perform()
 logger.debug(f"移動先座標: ({click_x}, {click_y})")
-
-# # JavaScriptを使用してクリック位置にマーカーを表示
-# although this is useful, it interacts ocr quite a bit since it just on top of the caracters
-# driver.execute_script("""
-#     var marker = document.createElement('div');
-#     marker.style.position = 'absolute';
-#     marker.style.background = 'red';
-#     marker.style.width = '20px';
-#     marker.style.height = '20px';
-#     marker.style.borderRadius = '50%';
-#     marker.style.left = arguments[0] + 'px';
-#     marker.style.top = arguments[1] + 'px';
-#     marker.style.zIndex = '1000';  // マーカーを前面に表示
-#     document.body.appendChild(marker);
-# """, click_x, click_y)
 logger.debug("JavaScriptを使用してクリック位置にマーカーを表示しました。")
 
 # 実際にクリックを実行
@@ -98,16 +80,15 @@ target_xpath = '/html/body'
 element = driver.find_element(By.XPATH, target_xpath)
 element.send_keys(" ")
 
-# 画像の範囲を指定するためのリスト
-im_ranges = [136, 124, 112, 100, 88, 76]
-
 # PyOCRのツール
-# tesseractが必要になる。参考: https://dev.classmethod.jp/articles/ocr-on-a-mac-device-with-pytesseract/
+# tesseractが必要になる。
+# 参考: https://dev.classmethod.jp/articles/ocr-on-a-mac-device-with-pytesseract/
 tool = pyocr.get_available_tools()[0]
 
 from time import time
 start = time()
-while time() - start < 90.0:
+# Since this is just for fun, we will only run this for 30 seconds
+while time() - start < 30.0:
 
     # 移動した
     # ファイル名
@@ -117,30 +98,15 @@ while time() - start < 90.0:
 
     # 画像をPILのImageを使って読み込む
     # ローマ字の部分を取り出す
-    # im = Image.open(fname).crop((0,230,500,254))
-    # this depends on the screen size, this didn't work with bigger screens. 
-    # Need modification for production
     im = Image.open(fname).crop((530, 690, 1030, 744))
-
-    # 画像の範囲を指定する
-    for im_range in im_ranges:
-        if im.getpixel((im_range, 0)) == (255, 255, 255, 255):
-            im = im.crop((im_range+20, 0, 500-im_range-20, 24))
-
-            # 画像を二値化する
-            im = im.convert("L")
-            for i in range(im.size[0]):
-                for j in range(im.size[1]):
-                    if im.getpixel((i, j)) >= 128:
-                        im.putpixel((i, j), 0)
-                    else:
-                        im.putpixel((i, j), 255)
-            break
-
     im.save("sample.png")
 
     # tool で文字を認識させる
-    text = tool.image_to_string(im, lang='eng', builder=pyocr.builders.TextBuilder())
+    text = tool.image_to_string(
+        im,
+        lang='eng',
+        builder=pyocr.builders.TextBuilder()
+    )
 
     # text を確認
     print(text)
